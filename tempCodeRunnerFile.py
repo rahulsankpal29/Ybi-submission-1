6fdd5424955f07b926616cbc44250680ef9aad25
rahulsankpal29 <rahulds2909@gmail.com>
2024-09-13 13:38:25 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
